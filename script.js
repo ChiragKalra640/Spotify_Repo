8e3c37320754a5794e423864d1fba8d3a1193742
ChiragKalra640 <116939460+ChiragKalra640@users.noreply.github.com>
2023-05-07 19:48:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("Welcome to Spotify");
+
+//Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+
+
+let songs = [
+    {songName: "Salam-e-Ishq",filePath:"songs/1.mps",coverPath:"covers/1.jpg"},
+    {songName: "Salam-e-Ishq",filePath:"songs/1.mps",coverPath:"covers/1.jpg"},
+    {songName: "Salam-e-Ishq",filePath:"songs/1.mps",coverPath:"covers/1.jpg"},
+    {songName: "Salam-e-Ishq",filePath:"songs/1.mps",coverPath:"covers/1.jpg"},
+    {songName: "Salam-e-Ishq",filePath:"songs/1.mps",coverPath:"covers/1.jpg"},
+    {songName: "Salam-e-Ishq",filePath:"songs/1.mps",coverPath:"covers/1.jpg"},
+]
+
+
+// audioElement.play();
+
+//Handle play/pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-play-circle');
+
+    }
+})
+
+//Listen to Event
+myProgressBar.addEventListener('timeupdate',()=>{
+    console.log('timeupdate');
+    //Update Seekbar
+})
